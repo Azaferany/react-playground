ab610a73c39c4c95e9e926ecb85b0eddc9931da2
ali zaferany <ali.zaferany79@gmail.com>
2023-03-10 18:18:12 +0330

    feat: add react-router-dom + Routes + CommonLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CommonLayout from "./layouts/CommonLayout";
+import UserHome from "./features/Users/Home";
+import ProductHome from "./features/Products/Home";
+import EditUser from "./features/Users/Edit";
+import EditProduct from "./features/Products/Edit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CommonLayout />}>
+          <Route index path="/users" element={<UserHome />} />
+          <Route index path="/users/edit/:id" element={<EditUser />} />
+          <Route index path="/products" element={<ProductHome />} />
+          <Route index path="/products/edit/:id" element={<EditProduct />} />
+          <Route index path="/verification" element={<h1 />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/layouts/CommonLayout.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Outlet, useLocation, Link } from "react-router-dom";
+import { Layout, Menu, Breadcrumb } from "antd";
+const { Header, Content, Footer } = Layout;
+
+const CommonLayout = () => {
+  let location = useLocation();
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={[location.pathname.toLowerCase()]}
+          style={{ lineHeight: "64px" }}
+        >
+          <Menu.Item key="/verification">
+            <Link to={"/verification"}>Verification</Link>
+          </Menu.Item>
+          <Menu.Item key="/users">
+            <Link to={"/users"}>Users</Link>
+          </Menu.Item>
+          <Menu.Item key="/products">
+            <Link to={"/products"}>Products</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "16px 50px" }}>
+        <Outlet />
+      </Content>
+
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+
+export default CommonLayout;
